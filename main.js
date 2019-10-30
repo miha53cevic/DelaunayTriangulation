1314236414f1b1d600d78c26c0498868bb30a6a7
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2019-10-30 20:10:25 +0100

    cleanup

--- main.js
+++ main.js
@@ -3,23 +3,30 @@ window.onload = () => {
 
     const triangulation = new Delunay_Triangulation();
 
+    // Array of points that will be added to the Triangulation
     let points = [];
 
     document.getElementById('canvas').addEventListener('click', () => {
         points.push(MOUSE_POS);
-        
+
+        // Clear screen
         clear('white');
 
+        // Draw Points
         for (let point of points) {
             drawFillRect(point.x - 2.5, point.y - 2.5, 5, 5, 'red');
         }
         
         let triangles = triangulation.RunAlgorithm(points);
+
+        // Draw the triangles
+        // TODO - filter the edges and draw them only once
         for (let triangle of triangles) {
-            triangle.drawFill('cyan', true);
+            triangle.draw();
+            //triangle.drawFill('cyan', true);
             //triangle.drawCircumcircle();
         }
 
-        console.log(triangles.length);
+        console.log(`Number of triangles: ${triangles.length}`);
     });
 };
